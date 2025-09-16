66d68ce720d79e95e118a78ac2d06a3803797b66
STOLENBOATS <almeida.ricardojorge@gmail.com>
2025-09-16 23:22:42 +0100

    Add files via upload

--- js/idmar-session.js
+++ js/idmar-session.js
@@ -1,9 +1,2 @@
-/*! IDMAR session gate r2 */
-(function(){
-  try {
-    var p = location.pathname;
-    var isLogin = /(?:^|\/)(login\.html)?$/i.test(p) || /(?:^|\/)index\.html$/i.test(p) && !/validador\.html|historico|forense\.html/i.test(p);
-    var has = sessionStorage.getItem('IDMAR_SESSION') || sessionStorage.getItem('NAV_SESSION');
-    if (!has && !isLogin) { location.replace('login.html'); }
-  } catch(e) { /* ignore */ }
-})();
+/* gate */
+(function(){try{var p=location.pathname;var isLogin=/(?:^|\/)(login\.html|index\.html)?$/i.test(p)&&!/validador|historico|forense/i.test(p);var has=sessionStorage.getItem('IDMAR_SESSION')||sessionStorage.getItem('NAV_SESSION');if(!has&&!isLogin){location.replace('login.html');}}catch(e){}})();
