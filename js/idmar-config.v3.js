598027ce7e1f938d7487fb684848524317132d8a
STOLENBOATS <almeida.ricardojorge@gmail.com>
2025-10-13 16:14:18 +0100

    idmar-config.v3.js

--- js/idmar-config.v3.js
+++ js/idmar-config.v3.js
@@ -1,3 +1,1 @@
-ï»¿(function(w){w.IDMAR_APP_NAME="IDMAR";w.IDMAR_APP_SUB="Identificaï¿½ï¿½o Marï¿½tima ï¿½ Cascos & Motores";})(window);
-
-
+(function(w){w.IDMAR_APP_NAME="IDMAR";w.IDMAR_APP_SUB="Identificação Marítima — Cascos & Motores";})(window);
